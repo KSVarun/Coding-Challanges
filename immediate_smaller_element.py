febb62f488e45fffef031bdc98bf6c545e71fa0d
KSVarun <ksvarun001@gmail.com>
2019-06-21 15:28:15 +0530

    immediate_smaller_element array modification

--- immediate_smaller_element.py
+++ immediate_smaller_element.py
@@ -7,20 +7,27 @@ k = 0
 
 
 def immediate_small(array_list, array_length):
+    output_array = []
     for k in range(array_length):
         try:
-            if(array_list[k+1] < array_list[k]):
-                print(array_list[k+1])
+            if(int(array_list[k+1]) < int(array_list[k])):
+                #print(int(array_list[k+1]), sep=' ')
+                output_array.append(int(array_list[k+1]))
             else:
-                print(-1)
+                #print(-1, sep=' ')
+                output_array.append(int(-1))
         except:
-            print(-1)
+            #print(-1, sep=' ')
+            output_array.append(int(-1))
+    print(*output_array)
     return
 
 
 for i in range(test_cases):
     array_length = int(input())
     array_list = []
-    for j in range(array_length):
-        array_list.append(int(input()))
+    array_input = input()
+    array_list = array_input.split(" ")
+    # for j in range(array_length):
+    #    array_list.append(int(input()))
     immediate_small(array_list, array_length)
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-i = 0
-for i in range(10):
-    print(i)
+arr = [1, 2, 3, 4, 5]
+
+print(*arr)
